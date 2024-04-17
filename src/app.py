afef68aaa7d7fa0765bb7bfe842ebd8d9b5ed454
guimy <guimingyue@gmail.com>
2024-04-17 09:21:07 +0800

    convert audio to text and reverse vice versa

--- src/app.py
+++ src/app.py
@@ -2,19 +2,17 @@ from melo.api import TTS
 from transformers import pipeline
 import numpy as np
 import gradio as gr
+import soundfile as sf
+import io
 
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
-def transcribe(stream, new_chunk):
-    sr, y = new_chunk
+
+def transcribe(audio):
+    sr, y = audio
     y = y.astype(np.float32)
     y /= np.max(np.abs(y))
 
-    if stream is not None:
-        stream = np.concatenate([stream, y])
-    else:
-        stream = y
-    text = transcriber({"sampling_rate": sr, "raw": stream})["text"]
-
+    text = transcriber({"sampling_rate": sr, "raw": y})["text"]
     print("text is: " + text)
     language='EN'
     accent='EN-US'
@@ -26,16 +24,19 @@ def transcribe(stream, new_chunk):
     speaker_ids = melo_model.hps.data.spk2id
     audio = melo_model.tts_to_file(text, speaker_ids[accent], None, speed=1.0)
     print("convert success," + text)
-    return audio, text
+    audio_buf = io.BytesIO()
+    audio_buf.name = 'file.wav'
+    sf.write(audio_buf, audio, melo_model.hps.data.sampling_rate)
+    audio_buf.seek(0)  # Necessary for `.read()` to return all bytes
+    return audio_buf.read()
 
 
 def main():
     print('ready to start app')
     demo = gr.Interface(
         transcribe,
-        ["state", gr.Audio(sources=["microphone"], streaming=True)],
-        ["state", "audio"],
-        live=True,
+        gr.Audio(sources=["microphone"]),
+        "audio",
     )
     demo.launch()
     print('ready to start')
