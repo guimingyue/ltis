8f51940215952be2db67b3bc7cd72e4b48f3beef
guimy <guimingyue@gmail.com>
2024-04-17 23:46:09 +0800

    add llm

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import numpy as np
 import gradio as gr
 import soundfile as sf
 import io
+import dashscope
+from http import HTTPStatus
 
 transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
 
@@ -13,22 +15,44 @@ def transcribe(audio):
     y /= np.max(np.abs(y))
 
     text = transcriber({"sampling_rate": sr, "raw": y})["text"]
-    print("text is: " + text)
+    print("src text is: " + text)
+
+    text, status = call_with_messages(text)
+    if not status:
+        return 
+
+    print("text from llm:" + text)
+
     language='EN'
     accent='EN-US'
-
     # CPU is sufficient for real-time inference.
     # You can set it manually to 'cpu' or 'cuda' or 'cuda:0' or 'mps'
     device = 'auto' # Will automatically use GPU if available
     melo_model = TTS(language=language, device=device)
     speaker_ids = melo_model.hps.data.spk2id
     audio = melo_model.tts_to_file(text, speaker_ids[accent], None, speed=1.0)
-    print("convert success," + text)
     audio_buf = io.BytesIO()
     audio_buf.name = 'file.wav'
     sf.write(audio_buf, audio, melo_model.hps.data.sampling_rate)
     audio_buf.seek(0)  # Necessary for `.read()` to return all bytes
     return audio_buf.read()
+
+'''
+you need to save api key
+'''
+def call_with_messages(text):
+    messages = [{'role': 'system', 'content': 'You are an American assistant who speak American English.'},
+                {'role': 'user', 'content': text}]
+
+    response = dashscope.Generation.call(
+        dashscope.Generation.Models.qwen_turbo,
+        messages=messages,
+        result_format='message',  # set the result to be "message" format.
+    )
+    if response.status_code == HTTPStatus.OK:
+        return response['output']['choices'][0]['message']['content'], True
+    else:
+        return 'EOF ERROR', False
 
 
 def main():
