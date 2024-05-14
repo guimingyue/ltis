76850a081e09d5a0a5cc41168392806b14ff0fe7
guimy <guimingyue@gmail.com>
2024-05-14 23:13:40 +0800

    receive audio from web audio through flask

--- src/web.py
+++ src/web.py
@@ -1,21 +1,30 @@
-import json
+
 from flask import Flask, request
 from chat import Chat
+from pydub import AudioSegment
+import numpy as np
+from datetime import datetime
 
 
 app = Flask(__name__)
 chat = Chat('chinese', 'qwen_turbo', '你是一个知识渊博的助手', 'base', 'ZH')
 
-@app.route("/api")
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.post('/api/media')
 def media():
     file = request.files['audio']
-    speech, text = chat.transcribe(file)
+    audio = AudioSegment.from_file_using_temporary_files(file.stream, format='wave')
+    data = np.array(audio.get_array_of_samples())
+    if audio.channels > 1:
+        data = data.reshape(-1, audio.channels)
+    t = audio.frame_rate, data
+    speech, text = chat.transcribe(t)
     return {
         "speech": speech,
-        "text": text
+        "text": text,
+        "file_type": file.content_type,
     }
 
